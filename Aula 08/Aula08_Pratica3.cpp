3ce8edb84b53c814275f15b416c6bf291867936e
Renan Costa Filgueiras <renan2727@hotmail.com>
2021-09-27 20:31:10 -0300

    Complementação Aula

--- Aula 08/Aula08_Pratica3.cpp
+++ Aula 08/Aula08_Pratica3.cpp
@@ -1,11 +1,56 @@
-Escreva um programa que mostre dois candidatos a eleição de representante de turma (Escolha os nomes e números que desejarem). Faça uma leitura de 15 votos e mostre a quantidade de votos que cada candidato recebeu e quem ganhou a eleição.
-
-Escreva um programa que leia o nome da pessoa e depois leia o sobrenome (somente 1 sobrenome, sem espaço) da pessoa (leituras separadas) e imprima o nome e sobrenome juntos.
-
-Desafio: Escreva um jogo de pedra, papel e tesoura. O sistema terá um menu com as três opção e mais a opção sair. Ao escolher uma opção o computador irá escolher também entre pedra, papel e tesoura e o sistema irá dizer quem venceu e voltar ao menu. Dica: para isso terá que gerar um número aleatório.
+/*Desafio: Escreva um jogo de pedra, papel e tesoura. O sistema 
+terá um menu com as três opção e mais a opção sair. 
+Ao escolher uma opção o computador irá escolher também entre pedra, papel e tesoura 
+e o sistema irá dizer quem venceu e voltar ao menu. Dica: 
+para isso terá que gerar um número aleatório.*/
 #include<stdio.h> //Biblioteca de entrada e saída de dados 
 #include<locale.h>//Biblioteca de definição de localidade
+#include<stdlib.h>
+#include<time.h>
 
 int main() { 
-	
+	srand(time(NULL));
 	setlocale(LC_ALL, "Portuguese");
+	
+	int opcao,opcaoSistema;
+
+	do {
+		printf("****PEDRA PAPEL TESOURA****");
+		printf("\nEscolha uma opção:\n1 - Pedra\n2 - Papel\n3 - Tesoura\nQualquer caractere - Sair\n");
+		scanf("%d",&opcao);
+		opcaoSistema = (rand()%3)+1;
+		
+		printf("Escolha do usuário: %d",opcao);
+		printf("\nEscolha do computador: %d",opcaoSistema);
+		
+		if(opcao==1){
+			if(opcaoSistema==1){
+				printf("\nEmpate!");
+			} else if(opcaoSistema==2){
+				printf("\nO computador ganhou!");
+			} else {
+				printf("\nO jogador ganhou!");
+			}
+		} else if(opcao==2) {
+			if(opcaoSistema==1){
+				printf("\nO jogador ganhou!");
+			} else if(opcaoSistema==2){	
+				printf("\nEmpate!");
+			} else {
+				printf("\nO computador ganhou!");
+			}
+		} else if(opcao==3){
+			if(opcaoSistema==1){
+				printf("\nO computador ganhou!");
+			} else if(opcaoSistema==2){
+				printf("\nO jogador ganhou!");
+			} else {
+				printf("\nEmpate!");
+			}
+		} else {
+			break;
+		}
+		system("PAUSE");
+		system("cls");
+	}while(1);
+}
